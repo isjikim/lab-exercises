b63a44166f62cd40cd41f538c1aa222b23a1a0f9
Isaac Kim <isjikim@gmail.com>
2019-01-25 14:17:53 -0800

    lab participation

--- week-3/week-3.R
+++ week-3/week-3.R
@@ -4,28 +4,33 @@
 event_capacity <- c(60, 50, '100', 100)
 
 # Fix the possible error in a new variable event_capacity_new
-
+event_capacity_new <- c(60, 50, 100, 100)
+  
 # Create a vector of event names for each event 
-event_names <- 
+event_names <- c("seahawks", "mariners", "coachella", "concert")
   
 # Fix the error in creating this dataframe
-events <- data.frame(event_names, event_cost, event_capacity)
+# You need to either create or delete event_cost because object is not currently found
+event_cost <- c(200, 50, 400, 100 )
+# Change event_capacity to event_capacity_new
+events <- data.frame(event_names, event_cost, event_capacity_new)
 
 # Check the event_capacity column
-
 events$event_capacity
 
 # Try with event_capacity_new column
 events <- data.frame(event_names, event_capacity_new)
+events$event_capacity_new
 
 # Are there any differences between event_capacity and event_capacity)_new?
-# Answer here
+# Answer here: No there are not any differences between the two
 
 # Fix an error of adding columns to the dataframe
-events.event_attendance <- c(54, 43, 85, 90)
+# events.event_attendance <- c(54, 43, 85, 90)
+events$event_attendance <- c(54, 43, 85, 90)
 
 # Add a column called seats_left in the events dataframe
-events$seats_left <-
+events$seats_left <- c(1, 2, 3, 4)
   
   
   
